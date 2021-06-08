4e2033fc1dfc889aba174fa319b5be552df441e2
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2021-06-08 22:58:55 +0800

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1537,7 +1537,7 @@ class moderation(commands.Cog):
         if not discord.utils.get(ctx.guild.roles, id = 652869023599558656) in member.roles and not discord.utils.get(ctx.guild.roles, id = 817813676178407425) in member.roles and not discord.utils.get(ctx.guild.roles, id = 851768758267412480) in member.roles:
             return await ctx.send(f'`[ERR]` {ctx.author.mention}, `вам не доступна данная команда!`', delete_after = 5) 
 
-        if not discord.utils.get(ctx.guild.roles, id = 652869023599558656) in member.roles and not discord.utils.get(ctx.guild.roles, id = 817813676178407425) in member.roles:
+        if not discord.utils.get(ctx.guild.roles, id = 652869023599558656) in member.roles and not discord.utils.get(ctx.guild.roles, id = 817813676178407425) in member.roles and not discord.utils.get(ctx.guild.roles, id = 851768758267412480) in member.roles:
           return await ctx.send(f'`[ERR]` {ctx.author.mention}, `данный пользователь не является агентом поддержки!`', delete_after = 5)
         
         if moderr.count_documents({"id": member.id}) == 0 and moder.count_documents({"guild": 477547500232769536, "id": member.id}) == 0:
