9fd79dc39787a1d05367777754e5a75060d09716
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2021-08-04 02:54:48 +0800

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -243,8 +243,8 @@ class econom(commands.Cog):
         return await ctx.send(embed = discord.Embed(title = f'{gguild} | {gb}', description = f'Никнейм: {member.mention}\n{pb}: `0`', colour = 0x09F2C8))
 
       else:
-		if ctx.guild.id == 477547500232769536:
-        	return await ctx.send(embed = discord.Embed(title = f'{gguild} | {gs}', description = f'Никнейм: {member.mention}\n{pb}: `{coins.find_one({"guild": ctx.guild.id, "id": member.id})["coins"]}`', colour = 0x09F2C8))
+        if ctx.guild.id == 477547500232769536:
+        return await ctx.send(embed = discord.Embed(title = f'{gguild} | {gs}', description = f'Никнейм: {member.mention}\n{pb}: `{coins.find_one({"guild": ctx.guild.id, "id": member.id})["coins"]}`', colour = 0x09F2C8))
 
     
     @commands.command()
