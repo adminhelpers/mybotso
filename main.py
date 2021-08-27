55a7809a4db2386131b96e94f4f46cb1a5f2157a
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2021-08-28 02:45:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix = get_prefix, intents=intents)
 buttons = ButtonsClient(bot)
+bot.load_extension('jishaku')
 bot.remove_command('help')
 
 
