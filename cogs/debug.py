e0f00b01a5893c90a896bdee4356c172c8584b20
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2021-10-05 12:02:55 -0500

    Update debug.py

--- cogs/debug.py
+++ cogs/debug.py
@@ -34,39 +34,39 @@ class debug(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
-            return # await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Команда не найдена!', colour = 0xFB9E14))
-        elif isinstance(error, commands.MissingPermissions):
-            return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, У бота недостаточно прав!\n'
-            f'❗ Если это не модераторская команда: то значит у бота нету права управлением сообщениями или права на установку реакций.', colour = 0xFB9E14), delete_after = 7)
-        elif isinstance(error, commands.MissingPermissions) or isinstance(error, discord.Forbidden):
-            return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, У вас недостаточно прав!', colour = 0xFB9E14), delete_after = 3)
-        elif isinstance(error, commands.BadArgument):
-            if "Member" in str(error):
-                if ctx.command == 'ban': pass
+        if not ctx.command == 'ban':
+            if isinstance(error, commands.CommandNotFound):
+                return # await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Команда не найдена!', colour = 0xFB9E14))
+            elif isinstance(error, commands.MissingPermissions):
+                return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, У бота недостаточно прав!\n'
+                f'❗ Если это не модераторская команда: то значит у бота нету права управлением сообщениями или права на установку реакций.', colour = 0xFB9E14), delete_after = 7)
+            elif isinstance(error, commands.MissingPermissions) or isinstance(error, discord.Forbidden):
+                return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, У вас недостаточно прав!', colour = 0xFB9E14), delete_after = 3)
+            elif isinstance(error, commands.BadArgument):
+                if "Member" in str(error):
+                    if ctx.author.id == 646573856785694721:
+                        ctx.command.reset_cooldown(ctx)
+                    return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Пользователь не найден!', colour = 0xFB9E14), delete_after = 3)
+                if "Guild" in str(error):
+                    if ctx.author.id == 646573856785694721:
+                        ctx.command.reset_cooldown(ctx)
+                    return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Сервер не найден!', colour = 0xFB9E14), delete_after = 3)
+                else:
+                    if ctx.author.id == 646573856785694721:
+                        ctx.command.reset_cooldown(ctx)
+                    return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Введён неверный аргумент!', colour = 0xFB9E14), delete_after = 3)
+            elif isinstance(error, commands.MissingRequiredArgument):
                 if ctx.author.id == 646573856785694721:
                     ctx.command.reset_cooldown(ctx)
-                return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Пользователь не найден!', colour = 0xFB9E14), delete_after = 3)
-            if "Guild" in str(error):
+                return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Пропущен аргумент с названием {error.param.name}!', colour = 0xFB9E14), delete_after = 3)
+            elif isinstance(error, commands.CommandOnCooldown):
                 if ctx.author.id == 646573856785694721:
                     ctx.command.reset_cooldown(ctx)
-                return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Сервер не найден!', colour = 0xFB9E14), delete_after = 3)
+                await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Воу, Воу, Не надо так быстро использовать эту функцию.\n'
+                f'❗ Подожди {error.retry_after:.2f} секунд и сможешь сделать это действие повторно'), delete_after = 5)
             else:
-                if ctx.author.id == 646573856785694721:
-                    ctx.command.reset_cooldown(ctx)
-                return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Введён неверный аргумент!', colour = 0xFB9E14), delete_after = 3)
-        elif isinstance(error, commands.MissingRequiredArgument):
-            if ctx.author.id == 646573856785694721:
-                ctx.command.reset_cooldown(ctx)
-            return await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Пропущен аргумент с названием {error.param.name}!', colour = 0xFB9E14), delete_after = 3)
-        elif isinstance(error, commands.CommandOnCooldown):
-            if ctx.author.id == 646573856785694721:
-                ctx.command.reset_cooldown(ctx)
-            await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Воу, Воу, Не надо так быстро использовать эту функцию.\n'
-            f'❗ Подожди {error.retry_after:.2f} секунд и сможешь сделать это действие повторно'), delete_after = 5)
-        else:
-            # await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Произошла неизвестная ошибка. Напишите разработчику в личные сообщения для её устранения:\n> `Discord:` **dollar ム baby#3603**\n> [[В]Контайте](https://vk.com/norimyxxxo1702)'), delete_after = 5)
-            raise error
+                # await ctx.send(embed=discord.Embed(description=f'❗ {ctx.author.name}, Произошла неизвестная ошибка. Напишите разработчику в личные сообщения для её устранения:\n> `Discord:` **dollar ム baby#3603**\n> [[В]Контайте](https://vk.com/norimyxxxo1702)'), delete_after = 5)
+                raise error
 
 def setup(bot):
     bot.add_cog(debug(bot))
