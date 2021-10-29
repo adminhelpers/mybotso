29f0a1aff1c2683d4cc86fcb705576988b03ab98
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2021-10-29 06:52:52 +0500

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -683,6 +683,7 @@ class econom(commands.Cog):
                         print(user_list_copy)
                         index_win += 1
                         index_coins = user_list_copy.index(index_massive)
+                        user_list_copy.pop(index_coins)
                         if index_win == 1:
                             message = f'🥇 **{user_list[index_coins]}**\n    `•` {user_request[index_coins]}\n\n'
                         elif index_win == 2:
@@ -693,7 +694,6 @@ class econom(commands.Cog):
                             message = f'`{index_win}.` **{user_list[index_coins]}**\n    `•` {user_request[index_coins]}\n\n'
                         win_content.append(message)
                         user_messages.pop(index_coins)
-                        user_list_copy.pop(index_coins)
                         user_list.pop(index_coins)
                         user_request.pop(index_coins)
                         if index_win == 10: break
