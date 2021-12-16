abaacccde0ec316c065587408ca7fd00ea684327
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2021-12-17 01:03:11 +0700

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -525,8 +525,8 @@ class econom(commands.Cog):
       else:
         tens.append(ctx.author.id)
         await ctx.send(embed = discord.Embed(title = f'Северный Округ | {gb}', description = f'**{ctx.author.mention}, Отдохни минутку и получишь результат!**', colour = 0x09F2C8))
-        a = random.choice([1, 2, 3, 4, 5])
-        if a != 2:
+        a = random.randint(1, 2)
+        if a == 1:
             await asyncio.sleep(5)
             bal = rebt(ctx.guild.id, ctx.author, amount)
             await ctx.send(embed = discord.Embed(title = f'{gguild} | {gb}', description = f'**{ctx.author.mention}, к сожалению, вы проиграли!\nТеперь Ваш баланс составляет: `{bal}` {pb}!**', colour = 0xff0000))
