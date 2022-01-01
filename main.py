aa3eb50416aeb8ed51005fab8be7d80daab81753
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2022-01-01 20:42:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ async def shop(ctx):
 
 @bot.command()
 async def reportmessage(ctx):
-await bot.get_channel(817815183094448130).send(embed = discord.Embed(description = 'h'))
+    await bot.get_channel(817815183094448130).send(embed = discord.Embed(description = 'h'))
 
 @bot.command()
 async def avatar(ctx, member: discord.Member = None):
