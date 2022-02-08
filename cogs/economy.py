ece3903c63396cd234f8508b6e69bb51df16a54a
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2022-02-08 16:27:56 +0700

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -184,7 +184,7 @@ class econom(commands.Cog):
             emoji, channel = str(payload.emoji), self.bot.get_channel(payload.channel_id)
             if not channel.id == 902666153674035280: return
             message = await channel.fetch_message(payload.message_id)
-            if not message.id == 915006300734251048: return
+            if not message.id == 915017576508194867: return
             memb = discord.utils.get(message.guild.members, id=payload.user_id)   
             if emoji == '❄️': return await memb.remove_roles(discord.utils.get(guild.roles, id = 915006300734251048))
 		
