b4f02b61a5b9af632c72b357b3cbfa755c35e0b2
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2022-04-15 20:56:20 +0500

    Update golosovanie.py

--- cogs/golosovanie.py
+++ cogs/golosovanie.py
@@ -47,9 +47,11 @@ class golosovanie(commands.Cog):
 
     @commands.command(aliases = ["перенеси"])
     async def moving(self, ctx, member: discord.Member):
+        print('+')
         kzam = discord.utils.get(ctx.guils.channels, name = "Кандидат на Заместителя")
         klid = discord.utils.get(ctx.guils.channels, name = "Обзвон [НЕ МЕШАТЬ]")
-        embed = discord.Embed(title = '\⛩️ **__Переносчик пользователей__**', description = f'{ctx.author}, вы находитесь в настройках переноса катигории **__СОБЕСЕДОВАНИЕ ЗАМКА__**.\nУкажите, в какой канал необходимо перенести пользователя:\n1️⃣ - {klid}\n2️⃣ - {kzam}')
+        print('+ 2')
+        embed = discord.Embed(title = '\⛩️ **__Переносчик пользователей__**', description = f'{ctx.author}, вы находитесь в настройках переноса катигории **__СОБЕСЕДОВАНИЕ ЗАМКА__**.\nУкажите, в какой канал необходимо перенести пользователя:\n1️⃣ - {klid.mention}\n2️⃣ - {kzam.meniton}')
         embed.set_footer(text = f'Support Team by dollar ム baby#7641', icon_url = 'https://images-ext-1.discordapp.net/external/cVW5pAsyoLnQiTP-DZzQ3hLnIq-2Kw3rBZUVZ33Cz30/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/729309765431328799/684fd7878d39ba93511700dbf7a45931.webp?width=677&height=677')
         message = await ctx.send(f'{ctx.author.mention}', embed = embed, delete_after = 30)
         await message.add_reaction('1️⃣')
@@ -66,7 +68,7 @@ class golosovanie(commands.Cog):
             elif str(react.emoji) == '2️⃣': 
                 schannel = kzam
             await member.remove_roles(discord.utils.get(ctx.guild.roles, id = 910232190175051887))
-            await ctx.send(f'{ctx.author.mention}', discord.Embed(title = '\⛩️ **__Переносчик пользователей__**', description = f'{ctx.author}, вы успешно переместили пользователя {member.name}`({member.mention})` в голосовой канал {schannel}'))
+            await ctx.send(f'{ctx.author.mention}', discord.Embed(title = '\⛩️ **__Переносчик пользователей__**', description = f'{ctx.author}, вы успешно переместили пользователя {member.name}`({member.mention})` в голосовой канал {schannel.mention}'))
 
     @commands.command()
     async def голосование(self, ctx):
